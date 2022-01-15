e174f6bb8cfc8bd89502f2ad8f19b73b5e9dea4e
Farid Hidayat <e.faridhidayat@gmail.com>
2022-01-16 03:08:56 +0700

    feat: add new 2 methods to check and get model class

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -10,4 +10,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function checkIfModelExist($model, $namespace = 'App') {
+        try {
+            $model = app("{$namespace}\Models\\{$model}");
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    protected function getModelClass($model, $namespace = 'App') {
+        try {
+            $model = app("{$namespace}\Models\\{$model}");
+            return $model;
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
 }
