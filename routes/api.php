50900e1f43a67afebc62cc73eb0a479973ac7fca
Farid Hidayat <e.faridhidayat@gmail.com>
2022-01-16 23:32:10 +0700

    fix: reorder default routes to last order, it will not replace another route from another Laravel package

--- routes/api.php
+++ routes/api.php
@@ -2,35 +2,41 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-$version = config('app.API_VERSION', 'v1');
+// NOTE: make sure this route default order at last
+app()->booted(function() {
 
-Route::namespace('SaltLaravel\Controllers')
-    ->middleware(['api'])
-    ->prefix("api/{$version}")
-    ->group(function () {
+  $version = config('app.API_VERSION', 'v1');
 
-  // DEFAULT: API RESOURCES
-  Route::get("{collection}", 'ApiResourcesController@index'); // get entire collection
-  Route::post("{collection}", 'ApiResourcesController@store'); // create new collection
+  Route::namespace('SaltLaravel\Controllers')
+      ->middleware(['api'])
+      ->prefix("api/{$version}")
+      ->group(function () {
 
-  Route::get("{collection}/trash", 'ApiResourcesController@trash'); // trash of collection
+    // DEFAULT: API RESOURCES
+    Route::get("{collection}", 'ApiResourcesController@index'); // get entire collection
+    Route::post("{collection}", 'ApiResourcesController@store'); // create new collection
 
-  Route::post("{collection}/import", 'ApiResourcesController@import'); // import collection from external
-  Route::post("{collection}/export", 'ApiResourcesController@export'); // export entire collection
-  Route::get("{collection}/report", 'ApiResourcesController@report'); // report collection
+    Route::get("{collection}/trash", 'ApiResourcesController@trash'); // trash of collection
 
-  Route::get("{collection}/{id}/trashed", 'ApiResourcesController@trashed')->where('id', '[a-zA-Z0-9]+'); // get collection by ID from trash
+    Route::post("{collection}/import", 'ApiResourcesController@import'); // import collection from external
+    Route::post("{collection}/export", 'ApiResourcesController@export'); // export entire collection
+    Route::get("{collection}/report", 'ApiResourcesController@report'); // report collection
 
-  // RESTORE data by ID (id), selected IDs (selected), and All data (all)
-  Route::post("{collection}/{id}/restore", 'ApiResourcesController@restore')->where('id', '[a-zA-Z0-9]+'); // restore collection by ID
+    Route::get("{collection}/{id}/trashed", 'ApiResourcesController@trashed')->where('id', '[a-zA-Z0-9]+'); // get collection by ID from trash
 
-  // DELETE data by ID (id), selected IDs (selected), and All data (all)
-  Route::delete("{collection}/{id}/delete", 'ApiResourcesController@delete')->where('id', '[a-zA-Z0-9]+'); // hard delete collection by ID
+    // RESTORE data by ID (id), selected IDs (selected), and All data (all)
+    Route::post("{collection}/{id}/restore", 'ApiResourcesController@restore')->where('id', '[a-zA-Z0-9]+'); // restore collection by ID
 
-  Route::get("{collection}/{id}", 'ApiResourcesController@show')->where('id', '[a-zA-Z0-9]+'); // get collection by ID
-  Route::put("{collection}/{id}", 'ApiResourcesController@update')->where('id', '[a-zA-Z0-9]+'); // update collection by ID
-  Route::patch("{collection}/{id}", 'ApiResourcesController@patch')->where('id', '[a-zA-Z0-9]+'); // patch collection by ID
-  // DESTROY data by ID (id), selected IDs (selected), and All data (all)
-  Route::delete("{collection}/{id}", 'ApiResourcesController@destroy')->where('id', '[a-zA-Z0-9]+'); // soft delete a collection by ID
+    // DELETE data by ID (id), selected IDs (selected), and All data (all)
+    Route::delete("{collection}/{id}/delete", 'ApiResourcesController@delete')->where('id', '[a-zA-Z0-9]+'); // hard delete collection by ID
+
+    Route::get("{collection}/{id}", 'ApiResourcesController@show')->where('id', '[a-zA-Z0-9]+'); // get collection by ID
+    Route::put("{collection}/{id}", 'ApiResourcesController@update')->where('id', '[a-zA-Z0-9]+'); // update collection by ID
+    Route::patch("{collection}/{id}", 'ApiResourcesController@patch')->where('id', '[a-zA-Z0-9]+'); // patch collection by ID
+    // DESTROY data by ID (id), selected IDs (selected), and All data (all)
+    Route::delete("{collection}/{id}", 'ApiResourcesController@destroy')->where('id', '[a-zA-Z0-9]+'); // soft delete a collection by ID
+
+  });
 
 });
+
