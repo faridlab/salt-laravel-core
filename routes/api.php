8078c5c7c4e525d88633a69199db5e0ddeae189c
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-13 23:11:08 +0700

    fix: api routes id pattern

--- routes/api.php
+++ routes/api.php
@@ -22,19 +22,19 @@ app()->booted(function() {
     Route::post("{collection}/export", 'ApiResourcesController@export'); // export entire collection
     Route::get("{collection}/report", 'ApiResourcesController@report'); // report collection
 
-    Route::get("{collection}/{id}/trashed", 'ApiResourcesController@trashed')->where('id', '[a-zA-Z0-9]+'); // get collection by ID from trash
+    Route::get("{collection}/{id}/trashed", 'ApiResourcesController@trashed')->where('id', '[a-zA-Z0-9-]+'); // get collection by ID from trash
 
     // RESTORE data by ID (id), selected IDs (selected), and All data (all)
-    Route::post("{collection}/{id}/restore", 'ApiResourcesController@restore')->where('id', '[a-zA-Z0-9]+'); // restore collection by ID
+    Route::post("{collection}/{id}/restore", 'ApiResourcesController@restore')->where('id', '[a-zA-Z0-9-]+'); // restore collection by ID
 
     // DELETE data by ID (id), selected IDs (selected), and All data (all)
-    Route::delete("{collection}/{id}/delete", 'ApiResourcesController@delete')->where('id', '[a-zA-Z0-9]+'); // hard delete collection by ID
+    Route::delete("{collection}/{id}/delete", 'ApiResourcesController@delete')->where('id', '[a-zA-Z0-9-]+'); // hard delete collection by ID
 
-    Route::get("{collection}/{id}", 'ApiResourcesController@show')->where('id', '[a-zA-Z0-9]+'); // get collection by ID
-    Route::put("{collection}/{id}", 'ApiResourcesController@update')->where('id', '[a-zA-Z0-9]+'); // update collection by ID
-    Route::patch("{collection}/{id}", 'ApiResourcesController@patch')->where('id', '[a-zA-Z0-9]+'); // patch collection by ID
+    Route::get("{collection}/{id}", 'ApiResourcesController@show')->where('id', '[a-zA-Z0-9-]+'); // get collection by ID
+    Route::put("{collection}/{id}", 'ApiResourcesController@update')->where('id', '[a-zA-Z0-9-]+'); // update collection by ID
+    Route::patch("{collection}/{id}", 'ApiResourcesController@patch')->where('id', '[a-zA-Z0-9-]+'); // patch collection by ID
     // DESTROY data by ID (id), selected IDs (selected), and All data (all)
-    Route::delete("{collection}/{id}", 'ApiResourcesController@destroy')->where('id', '[a-zA-Z0-9]+'); // soft delete a collection by ID
+    Route::delete("{collection}/{id}", 'ApiResourcesController@destroy')->where('id', '[a-zA-Z0-9-]+'); // soft delete a collection by ID
 
   });
 
