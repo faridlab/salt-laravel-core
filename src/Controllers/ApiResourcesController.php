2318a2f16d03502ddb4cd6aa15660d5d825056da
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-27 20:59:51 +0700

    fix: restore issue about id

--- src/Controllers/ApiResourcesController.php
+++ src/Controllers/ApiResourcesController.php
@@ -416,6 +416,13 @@ class ApiResourcesController extends Controller
                 return $this->responder->response();
             }
 
+            $isUuid = Str::isUuid($idOrType);
+            if(!$isUuid) {
+              $this->responder->set('message', "Request method not defined");
+              $this->responder->setStatus(400, 'Bad Request.');
+              return $this->responder->response();
+            }
+
             // Pointing to spesific data by ID
             $model = $this->model->find($idOrType);
             if(is_null($model)) {
@@ -567,55 +574,57 @@ class ApiResourcesController extends Controller
         try {
             if(is_null($id)) $id = $collectionOrId;
 
-            $id = intval($id) > 0 ? intval($id): $id;
             // FIXME: if condition depth only 2
-            if(!is_int($id)) {
-                if($id == "selected") { // Delete all selected IDs
-                    if($request->has('selected')) {
-                        $ids = $request->get('selected');
-                        $model = $this->model->onlyTrashed()->whereIn('id', $ids);
-                        if($model->count() < 1) {
-                            $this->responder->set('message', 'Selected IDs not found');
-                            $this->responder->setStatus(404, 'Not Found');
-                            return $this->responder->response();
-                        }
-                        $model->restore();
-                        $this->responder->set('message', 'Selected IDs are restored');
-                        $this->responder->set('data', $model);
-                        return $this->responder->response();
-                    } else {
-                        $this->responder->set('message', "Selected IDs is required");
-                        $this->responder->setStatus(400, 'Bad Request.');
-                        return $this->responder->response();
-                    }
-                } else if($id == "all") { // Delete all selected
-                    $model = $this->model->onlyTrashed();
+            if($id == "selected") { // Delete all selected IDs
+                if($request->has('selected')) {
+                    $ids = $request->get('selected');
+                    $model = $this->model->onlyTrashed()->whereIn('id', $ids);
                     if($model->count() < 1) {
-                        $this->responder->set('message', 'There is not data found');
+                        $this->responder->set('message', 'Selected IDs not found');
                         $this->responder->setStatus(404, 'Not Found');
                         return $this->responder->response();
                     }
                     $model->restore();
-                    $this->responder->set('message', 'All data are restored');
+                    $this->responder->set('message', 'Selected IDs are restored');
                     $this->responder->set('data', $model);
                     return $this->responder->response();
-                } else {
-                    $this->responder->set('message', "Request method not defined");
-                    $this->responder->setStatus(400, 'Bad Request.');
-                    return $this->responder->response();
                 }
-            } else { // Pointing to spesific data by ID
-                $data = $this->model->onlyTrashed()->find($id);
-                if(is_null($data)) {
-                    $this->responder->set('message', 'Data not found');
+                $this->responder->set('message', "Selected IDs is required");
+                $this->responder->setStatus(400, 'Bad Request.');
+                return $this->responder->response();
+            }
+
+            if($id == "all") { // Delete all selected
+                $model = $this->model->onlyTrashed();
+                if($model->count() < 1) {
+                    $this->responder->set('message', 'There is not data found');
                     $this->responder->setStatus(404, 'Not Found');
                     return $this->responder->response();
                 }
-                $data->restore();
-                $this->responder->set('message', 'Data restored');
-                $this->responder->set('data', $data);
+                $model->restore();
+                $this->responder->set('message', 'All data are restored');
+                $this->responder->set('data', $model);
                 return $this->responder->response();
             }
+
+            $isUuid = Str::isUuid($id);
+            if(!$isUuid) {
+              $this->responder->set('message', "Request method not defined");
+              $this->responder->setStatus(400, 'Bad Request.');
+              return $this->responder->response();
+            }
+
+            // Pointing to spesific data by ID
+            $data = $this->model->onlyTrashed()->find($id);
+            if(is_null($data)) {
+                $this->responder->set('message', 'Data not found');
+                $this->responder->setStatus(404, 'Not Found');
+                return $this->responder->response();
+            }
+            $data->restore();
+            $this->responder->set('message', 'Data restored');
+            $this->responder->set('data', $data);
+            return $this->responder->response();
         } catch(\Exception $e) {
             $this->responder->set('message', $e->getMessage());
             $this->responder->setStatus(500, 'Internal server error.');
