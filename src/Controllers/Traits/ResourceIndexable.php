fd90a7409d7dce47c2e82bfe758437efb9f7509a
Farid Hidayat <e.faridhidayat@gmail.com>
2022-09-14 23:19:14 +0700

    refactor(Trait): ResourceIndexable

---
+++ src/Controllers/Traits/ResourceIndexable.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace SaltLaravel\Controllers\Traits;
+
+use Illuminate\Http\Request;
+
+trait ResourceIndexable
+{
+
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request) {
+
+      $this->checkModelAuthorization('index', 'read');
+
+      try {
+
+          $count = $this->model->count();
+          $model = $this->model->filter();
+
+          $format = $request->get('format', 'default');
+
+          $limit = intval($request->get('limit', 25));
+          if($limit > 100) {
+              $limit = 100;
+          }
+
+          $p = intval($request->get('page', 1));
+          $page = ($p > 0 ? $p - 1: $p);
+
+          if($format == 'datatable') {
+              $draw = $request['draw'];
+          }
+
+          $modelCount = clone $model;
+          $meta = array(
+              'recordsTotal' => $count,
+              'recordsFiltered' => $modelCount->count()?: $count
+          );
+
+          $data = $model
+                      ->offset($page * $limit)
+                      ->limit($limit)
+                      ->get();
+
+          $this->responder->set('message', 'Data retrieved.');
+          $this->responder->set('meta', $meta);
+          $this->responder->set('data', $data);
+          if($format == 'datatable') {
+              $this->responder->set('draw', $draw);
+              $this->responder->set('recordsFiltered', $meta['recordsFiltered']);
+              $this->responder->set('recordsTotal', $meta['recordsTotal']);
+          }
+          return $this->responder->response();
+      } catch(\Exception $e) {
+          $this->responder->set('message', $e->getMessage());
+          $this->responder->setStatus(500, 'Internal server error.');
+          return $this->responder->response();
+      }
+  }
+
+}
