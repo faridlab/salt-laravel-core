597fd3d13fabdefe0f7ce29474695a554e7ac6dd
Farid Hidayat <farid@startapp.id>
2025-02-09 23:22:54 +0700

    refactor: update namespace

--- src/Controllers/ApiResourcesNestedController.php
+++ src/Controllers/ApiResourcesNestedController.php
@@ -5,7 +5,7 @@ namespace SaltLaravel\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use SaltLaravel\Models\Resources;
-use Controller;
+use SaltLaravel\Controllers\Controller;
 use Illuminate\Support\Str;
 
 // EXCEPTIONS
